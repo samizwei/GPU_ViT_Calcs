704afcdc6215ac451261082ecbc9687cdd618cc7
samizwei <samiz@student.ethz.ch>
2025-04-18 00:03:18 +0200

    12x12 Vit with transflip done

--- XYZ_12x12Lattice_ViT/patching_xy44_signstructure/ViT_12x12_d32_nl1_SS_Second.py
+++ XYZ_12x12Lattice_ViT/patching_xy44_signstructure/ViT_12x12_d32_nl1_SS_Second.py
@@ -33,8 +33,9 @@ jax.devices()
 print("Sharding is enabled:", nk.config.netket_experimental_sharding)
 print("The available GPUs are:", jax.devices())
 
-
 nk.config.netket_random_state_fallback_warning = False
+
+
 
 
 
@@ -52,7 +53,7 @@ using the learning rate: linear_schedule(init_value=1e-3, end_value=1e-4, transi
 
 ######################################################################################################################################
 # define the sampler, Hamiltonian and grpah
-L = 14
+L = 12
 
 TriGraph = nk.graph.Triangular(extent = [L,L], pbc = True)
 
@@ -63,13 +64,12 @@ pHa = {
     'Dxy': 0.75,
     'd' : 0.1,
     'dprime' : 0.5,
-    'sublattice': [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,
-                   28,29,30,31,32,33,34,35,36,37,38,39,40,41,
-                   56,57,58,59,60,61,62,63,64,65,66,67,68,69,
-                   84,85,86,87,88,89,90,91,92,93,94,95,96,97,
-                   112,113,114,115,116,117,118,119,120,121,122,123,124,125,
-                   140,141,142,143,144,145,146,147,148,149,150,151,152,153,
-                   168,169,170,171,172,173,174,175,176,177,178,179,180,181]
+    'sublattice': [0,1,2,3,4,5,6,7,8,9,10,11,
+                   24,25,26,27,28,29,30,31,32,33,34,35,
+                   48,49,50,51,52,53,54,55,56,57,58,59,
+                   72,73,74,75,76,77,78,79,80,81,82,83,
+                   96,97,98,99,100,101,102,103,104,105,106,107,
+                   120,121,122,123,124,125,126,127,128,129,130,131]
 }
 
 Ha16, hi2d = H_afmJ123(L=pHa['L'], J1=pHa['J1'], J2=pHa['J2'], J3=pHa['J2'], Dxy=pHa['Dxy'], d=pHa['d'], dprime=pHa['dprime'], return_space=True,
@@ -84,25 +84,20 @@ sa_ex = nk.sampler.MetropolisExchange(hilbert=hi2d, graph=TriGraph, n_chains=32,
 
 rules5050 = nk.sampler.rules.MultipleRules(rules = [sa_Ha.rule, sa_ex.rule], probabilities = [0.5, 0.5])
 
-sa_HaEx5050 = nk.sampler.MetropolisSampler(hi2d, rules5050, n_chains=64, sweep_size=3*hi2d.size)
+sa_HaEx5050 = nk.sampler.MetropolisSampler(hi2d, rules5050, n_chains=128, sweep_size=3*hi2d.size)
 
 
 ######################################################################################################################################
-warmup_schedule = linear_schedule(init_value=1e-5, end_value=2*1e-3, transition_steps=50)
-
-decay_schedule = linear_schedule(init_value=2*1e-3, end_value=1e-5, transition_begin=200, transition_steps=300)
-
-lr_schedule = join_schedules(schedules=[warmup_schedule, decay_schedule], boundaries=[50] )
 
 p_opt = {
-    'learning_rate' : lr_schedule,
-    # 'learning_rate' : linear_schedule(init_value=0.5 * 1e-2, end_value = 1e-4, transition_begin=300, transition_steps=200),
+    # 'learning_rate' : lr_schedule,
+    'learning_rate' : linear_schedule(init_value=1e-4, end_value = 1e-6, transition_begin=0, transition_steps=200),
     # 'learning_rate': cosine_decay_schedule(init_value=1e-3, decay_steps = 100, alpha = 1e-2),
-    'diag_shift': 1e-4,
-    # 'diag_shift': linear_schedule(init_value=1e-4, end_value=1e-5, transition_begin=150, transition_steps=400),
+    # 'diag_shift': 1e-4,
+    'diag_shift': linear_schedule(init_value=1e-4, end_value=1e-5, transition_begin=100, transition_steps=100),
     'n_samples': 2**12,
     'chunk_size': 2**12,
-    'n_iter': 600,
+    'n_iter': 200,
 }
 
 pVit = {
@@ -112,21 +107,19 @@ pVit = {
     'Dtype': jnp.float64,
     'hidden_density': 1,
     'L': L,
-    'Cx': 7,
-    'Cy': 2,
-    'patch_arr': HashableArray(jnp.array([[0,1,14,15,28,29,42,43,56,57,70,71,84,85], [98,99,112,113,126,127,140,141,154,155,168,169,182,183],
-                                          [2,3,16,17,30,31,44,45,58,59,72,73,86,87], [100,101,114,115,128,129,142,143,156,157,170,171,184,185],
-                                          [4,5,18,19,32,33,46,47,60,61,74,75,88,89], [102,103,116,117,130,131,144,145,158,159,172,173,186,187],
-                                          [6,7,20,21,34,35,48,49,62,63,76,77,90,91], [104,105,118,119,132,133,146,147,160,161,174,175,188,189],
-                                          [8,9,22,23,36,37,50,51,64,65,78,79,92,93], [106,107,120,121,134,135,148,149,162,163,176,177,190,191],
-                                          [10,11,24,25,38,39,52,53,66,67,80,81,94,95], [108,109,122,123,136,137,150,151,164,165,178,179,192,193],
-                                          [12,13,26,27,40,41,54,55,68,69,82,83,96,97], [110,111,124,125,138,139,152,153,166,167,180,181,194,195],
+    'Cx': 4,
+    'Cy': 4,
+    'patch_arr': HashableArray(jnp.array([[0,1,2,3,12,13,14,15,24,25,26,27,36,37,38,39],
+                                          [4,5,6,7,16,17,18,19,28,29,30,31,40,41,42,43],
+                                          [8,9,10,11,20,21,22,23,32,33,34,35,44,45,46,47],
+                                          [48,49,50,51,60,61,62,63,72,73,74,75,84,85,86,87],
+                                          [52,53,54,55,64,65,66,67,76,77,78,79,88,89,90,91],
+                                          [56,57,58,59,68,69,70,71,80,81,82,83,92,93,94,95],
+                                          [96,97,98,99,108,109,110,111,120,121,122,123,132,133,134,135],
+                                          [100,101,102,103,112,113,114,115,124,125,126,127,136,137,138,139],
+                                          [104,105,106,107,116,117,118,119,128,129,130,131,140,141,142,143],
                                          ]))
-    
-    # HashableArray(jnp.array([[0,1,2,3,4,10,11,12,13,14,20,21,22,23,24,30,31,32,33,34,40,41,42,43,44],
-    #                                       [50,51,52,53,54,60,61,62,63,64,70,71,72,73,74,80,81,82,83,84,90,91,92,93,94],
-    #                                       [5,6,7,8,9,15,16,17,18,19,25,26,27,28,29,35,36,37,38,39,45,46,47,48,49],
-    #                                       [55,56,57,58,59,65,66,67,68,69,75,76,77,78,79,85,86,87,88,89,95,96,97,98,99]])),
+
 }
 
 
@@ -139,16 +132,42 @@ samplers = {
 # print('everything worked so far!!')
 
 # DataDir = '/scratch/samiz/GPU_ViT_Calcs/XYZ_10x10_Lattice_ViT/patching_xy55/Log_Files/'
-# DataDir = 'Log_Files/'
-DataDir = 'Log_Files_12x12_Params/'
+DataDir = 'Log_Files/'
 
 Stopper1 = InvalidLossStopping(monitor = 'mean', patience = 20)
 Stopper2 = LateConvergenceStopping(target = 0.005, monitor = 'variance', patience = 20, start_from_step=100)
 
-with open ('/scratch/samiz/GPU_ViT_Calcs/XYZ_12x12Lattice_ViT/patching_xy44_signstructure/Log_Files/log_vit_sampler_HaEx_5050.pickle', 'rb') as f:
+def get_tanslation(Lx, Ly, Ntot, px, py):
+    """
+    function to all translation of the lattice where tranlsation in y-direction is done by 2 sites
+    px, py are the sizes of the patch 
+
+    return: translations of whole lattice restricted to patch
+    """
+
+    assert Ntot == Lx * Ly, "The number of lattice nodes must be equal to the product of the number of lattice sites in x and y direction."
+
+    nodes = jnp.arange(0, Ntot)
+    transl = []
+
+    for i in range(px):
+        for j in range(0,py, 2):
+            transl.append(jnp.roll(nodes.reshape(-1, Ly), shift=j, axis=1).reshape(-1)) #translations in y direction jnp.roll(nodes, shift=, axis=0)
+        
+        nodes = jnp.roll(nodes, shift=Ly, axis=0) #translations in x direction
+
+    transl = jnp.array(transl)
+    assert transl.shape[0] == px * py / 2
+
+    return transl
+
+patch_transl = HashableArray(get_tanslation(L, L, L**2, pVit['Cx'], pVit['Cy']))
+
+with open('/scratch/samiz/GPU_ViT_Calcs/XYZ_12x12Lattice_ViT/patching_xy44_signstructure/Log_Files/log_vit_sampler_HaEx_5050.pickle', 'rb') as f:
     ps = pickle.load(f)
 
-print('ps:', ps['params']['Final_Complex_Layer_0'].keys())
+gparams = {'params' : {'ViT_2d_0': ps['params']}}
+
 
 for j, sa_key in enumerate(samplers.keys()):
 #     for _, dshift in enumerate(dshifts):
@@ -156,22 +175,17 @@ for j, sa_key in enumerate(samplers.keys()):
     print('curr sampler:', samplers[sa_key])
 
 #                 # define the model
-    m_Vit = vit.ViT_2d(patch_arr=HashableArray(pVit['patch_arr']), embed_dim=pVit['d'], num_heads=pVit['h'], nl=pVit['nl'],
-                                Dtype=pVit['Dtype'], L=pVit['L'], Cx=pVit['Cx'], Cy=pVit['Cy'], hidden_density=pVit['hidden_density'])
-                
+    m_Vit = vit.Vit_2d_full_symm(patch_arr=HashableArray(pVit['patch_arr']), embed_dim=pVit['d'], num_heads=pVit['h'], nl=pVit['nl'],
+                                Dtype=pVit['Dtype'], L=pVit['L'], Cx=pVit['Cx'], Cy=pVit['Cy'], hidden_density=pVit['hidden_density'],
+                                recover_full_transl_symm=True, translations = patch_transl, recover_spin_flip_symm=True)
     
     log_curr = nk.logging.RuntimeLog()
 
     gs_Vit, vs_Vit = VMC_SR(hamiltonian=Ha16.to_jax_operator(), sampler=samplers[sa_key], learning_rate=p_opt['learning_rate'], model=m_Vit,
-                                            diag_shift=p_opt['diag_shift'], n_samples=p_opt['n_samples'], chunk_size = p_opt['chunk_size'], discards = 16)
-    
-    init_params = vs_Vit.parameters
-    init_params['Final_Complex_Layer_0'] = ps['params']['Final_Complex_Layer_0']
-
-    gs_Vit.update_parameters(init_params)
-    # print(vs_Vit.parameters.keys())
-    # print(vs_Vit.parameters['Final_Complex_Layer_0'].keys())
-    StateLogger = PickledJsonLog(output_prefix=DataDir + 'log_vit_sampler_{}'.format(sa_key), save_params_every=10, save_params=True)
+                                            diag_shift=p_opt['diag_shift'], n_samples=p_opt['n_samples'], chunk_size = p_opt['chunk_size'], discards = 16,
+                                            parameters = gparams)
+                
+    StateLogger = PickledJsonLog(output_prefix=DataDir + 'log_vit_sampler_{}_transflip'.format(sa_key), save_params_every=10, save_params=True)
 
     # x,y = np.unique(np.sum(vs_Vit.samples.reshape(-1, L**2), axis=-1)/2, return_counts=True)
     # print(x, '\n', y)
